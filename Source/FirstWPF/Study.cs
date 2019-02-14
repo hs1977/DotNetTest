238a75af1b283a7333d7fa760796475f51a09f87
hs1977 <flower.excel@sk.com>
2019-02-14 20:55:06 +0900

    FirstVisual 추가

---
+++ Source/FirstWPF/Study.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWPF
+{
+
+	/*
+	// < Control Default Property >
+
+	1.	Button, Label, TextBox 등의 기본 Width, Height는 Content, Text의 내용과 글꼴 크기에 의해서 정해진다.
+		대부분 Content, Text를 적당히 감쌀 정도의 크기가 된다. Content, Text가 없을 경우 글꼴 크기에 의한 최소 크기로 정해진다.
+
+	2.	기본 Canvas.Top, Canvas.Left은 0이다. 
+
+	3.	Label, TextBox의 기본 HorizontalContentAlignment, VerticalContentAlignment는 Left, Top이다.
+		Button의 기본 HorizontalContentAlignment, VerticalContentAlignment는 모두 Center이다.
+
+	4.	WrapPanel에서 기본 Height는 해당 Row에서 가장 높이가 큰 Control의 Height를 나타낸다.
+
+	5.	WrapPanel의 ItemWidth와 ItemHeight는 자식 Control의 Width, Height를 제한한다.
+		기본 ItemWidth와 ItemHeight는 자식 Control의 Width, Height를 제한하지 않는다.
+
+	6.	StackPanel에서 기본 Width와 Height는 Orientation에 따라서 StackPanel의 Width와 Height가 된다.
+
+	7.	StackPanel에서 Orientation에 부합하는 HorizontalAlignment이나 VerticalAlignment이 무시된다.
+
+	8.	GridPanel에서 기본 Width와 Height는 Grid 각 Cell의 Width와 Height가 된다.
+
+
+	// < Panel Default Property >
+
+	1.	Panel의 기본 Width, Height는 HorizontalAlignment와 VerticalAlignment가 Stretch일 경우 부모(Ex. Window)의 Width, Height가 되며
+		Stretch가 아닐 경우 0이 된다.
+	*/
+
+
+	class Study
+	{
+	}
+}
+
