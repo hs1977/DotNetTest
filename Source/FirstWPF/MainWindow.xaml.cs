ecd778e570445e09845295e490f2afbfbad749c2
hs1977 <flower.excel@sk.com>
2019-02-15 20:18:38 +0900

    Library, App, Window 속성

--- Source/FirstWPF/MainWindow.xaml.cs
+++ Source/FirstWPF/MainWindow.xaml.cs
@@ -20,9 +20,68 @@ namespace FirstWPF
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		List<BitmapImage> images = new List<BitmapImage>();
+
+		private int currImage = 0;
+		private const int MAX_IMAGES = 6;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				string path = Environment.CurrentDirectory;
+
+				images.Add(new BitmapImage(new Uri(string.Format(@"{0}\Images\바다1.jpg", path))));
+				images.Add(new BitmapImage(new Uri(string.Format(@"{0}\Images\바다3.jpg", path))));
+				images.Add(new BitmapImage(new Uri(string.Format(@"{0}\Images\바다5.jpg", path))));
+
+				images.Add(new BitmapImage(new Uri(@"/Images/박보영3.jpg", UriKind.Relative)));
+				images.Add(new BitmapImage(new Uri(@"/Images/박보영4.jpg", UriKind.Relative)));
+				images.Add(new BitmapImage(new Uri(@"/Images/박보영5.jpg", UriKind.Relative)));
+
+				imageHolder.Source = images[currImage];
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void BtnPreviousImage_Click(object sender, RoutedEventArgs e)
+		{
+			if(--currImage < 0)
+			{
+				currImage = MAX_IMAGES - 1;
+			}
+				
+			imageHolder.Source = images[currImage];
+		}
+
+		private void BtnNextImage_Click(object sender, RoutedEventArgs e)
+		{
+			if(++currImage == MAX_IMAGES)
+			{
+				currImage = 0;
+			}
+				
+			imageHolder.Source = images[currImage];
+		}
+
+		private void BtnTest1_Click(object sender, RoutedEventArgs e)
+		{
+			Resources["ApplicationBrush"] = new SolidColorBrush(Colors.Red);
+			Resources["WindowBrush"] = new SolidColorBrush(Colors.Red);
+		}
+
+		private void BtnTest2_Click(object sender, RoutedEventArgs e)
+		{
+			Resources["FileBrush"] = new SolidColorBrush(Colors.Red);
+			Resources["LibraryBrush"] = new SolidColorBrush(Colors.Red);
 		}
 	}
 }
